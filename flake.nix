7294be3c39c708558ca897ed2c412bc7d96f83dc
Paul Leclerc <contact@paul-leclerc.fr>
2025-12-14 00:04:11 +0100

    Install openssl with direnv and flake.nix

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell rec {
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
+
+        buildInputs = with pkgs; [
+          openssl
+        ];
+      };
+    };
+}
