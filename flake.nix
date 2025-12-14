423d372bb0de7160f7c0d283ac13abf688bf2406
Paul Leclerc <contact@paul-leclerc.fr>
2025-12-14 03:47:37 +0100

    Add iced dependencies in flake.nix

--- flake.nix
+++ flake.nix
@@ -6,6 +6,19 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      dlopenLibraries = with pkgs; [
+        libxkbcommon
+
+        # GPU backend
+        vulkan-loader
+        # libGL
+
+        # Window system
+        wayland
+        # xorg.libX11
+        # xorg.libXcursor
+        # xorg.libXi
+      ];
     in
     {
       devShells.${system}.default = pkgs.mkShell rec {
@@ -16,6 +29,8 @@
         buildInputs = with pkgs; [
           openssl
         ];
+
+        env.RUSTFLAGS = "-C link-arg=-Wl,-rpath,${nixpkgs.lib.makeLibraryPath dlopenLibraries}";
       };
     };
 }
